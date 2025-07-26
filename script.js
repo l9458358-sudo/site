dc169bb3fbc3f81b3bbfd54693efbf42c06dd692
l9458358-sudo <l9458358@gmail.com>
2025-07-26 05:19:00 -0400

    Use Bootstrap and Google icons for modern layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('enroll-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    document.getElementById('success-message').classList.remove('hidden');
+    document.getElementById('success-message').classList.remove('d-none');
     this.reset();
 });
